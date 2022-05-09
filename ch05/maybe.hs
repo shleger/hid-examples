cde51d7095fac6b92c681d91ecdf59c1c8acd339
Andrey_Shleger <Andrey_Shleger@epam.com>
2022-05-10 00:05:02 +0300

    understood flip in monad expression

--- ch05/maybe.hs
+++ ch05/maybe.hs
@@ -1,20 +1,23 @@
 import Text.Read (readMaybe)
 
 type Name = String
+
 type Phone = String
+
 type Location = String
+
 type PhoneNumbers = [(Name, Phone)]
+
 type Locations = [(Phone, Location)]
 
 doubleStrNumber1 :: (Num a, Read a) => String -> Maybe a
 doubleStrNumber1 str =
   case readMaybe str of
-    Just x -> Just (2*x)
+    Just x -> Just (2 * x)
     Nothing -> Nothing
 
 doubleStrNumber2 :: (Num a, Read a) => String -> Maybe a
-doubleStrNumber2 s = (2*) `fmap` readMaybe s
-
+doubleStrNumber2 s = (* 2) <$> readMaybe s
 
 plusStrNumbers :: (Num a, Read a) => String -> String -> Maybe a
 plusStrNumbers s1 s2 = (+) <$> readMaybe s1 <*> readMaybe s2
@@ -22,6 +25,11 @@ plusStrNumbers s1 s2 = (+) <$> readMaybe s1 <*> readMaybe s2
 locateByName :: PhoneNumbers -> Locations -> Name -> Maybe Location
 locateByName pnumbers locs name =
   lookup name pnumbers >>= flip lookup locs
+-- ^ We’ve had to flip arguments for lookup to get a function suitable
+--   for the monadic binding (the flip lookup locs subexpression has
+--   the required Phone -> Maybe Location type)
+--  example:
+--   flip lookup locs "495"
 
 locateByName' :: PhoneNumbers -> Locations -> Name -> Maybe Location
 locateByName' pnumbers locs name =
@@ -34,4 +42,4 @@ main = do
   print (doubleStrNumber1 "21" :: Maybe Int)
   print (doubleStrNumber2 "21" :: Maybe Int)
   print (plusStrNumbers "10" "xx" :: Maybe Int)
-
+  print (plusStrNumbers "10" "20" :: Maybe Int)
