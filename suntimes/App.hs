406f1b96a4b871c719261f4ba5a8b8a5f5a731df
Andrey_Shleger <Andrey_Shleger@epam.com>
2022-05-16 10:47:54 +0300

    reformat and add MonadLogger to suntimes

--- suntimes/App.hs
+++ suntimes/App.hs
@@ -1,17 +1,27 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Eta reduce" #-}
 
 module App where
 
-import Control.Monad.Reader
 import Control.Monad.Catch
-
+import Control.Monad.Logger
+import Control.Monad.Reader
 import Types
 
-newtype MyApp a = MyApp {
-      runApp :: ReaderT WebAPIAuth IO a
-    } deriving (Functor, Applicative, Monad, MonadIO,
-                MonadThrow, MonadCatch, MonadMask,
-                MonadReader WebAPIAuth)
+newtype MyApp a = MyApp {runApp :: ReaderT WebAPIAuth (LoggingT IO) a}
+  deriving
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadIO,
+      MonadThrow,
+      MonadCatch,
+      MonadMask,
+      MonadReader WebAPIAuth,
+      MonadLogger
+    )
 
 runMyApp :: MyApp a -> WebAPIAuth -> IO a
-runMyApp app config = runReaderT (runApp app) config
+runMyApp app config = runStderrLoggingT (runReaderT (runApp app) config) --add runner for the logger
